8c9c6c196b62946bb5fb8d72d9649829a71354de
Scott McCulley <smcculley@gmail.com>
2022-10-03 14:12:57 -0700

    initial add of tank_monitor to repo

---
+++ tank_monitor/code.py
@@ -0,0 +1,71 @@
+import time
+import board
+import pwmio
+
+from digitalio import DigitalInOut, Direction, Pull
+from analogio import AnalogIn
+
+sensor_enable = DigitalInOut(board.A0) # Pin to power on the sensor
+sensor_enable.switch_to_output()
+sensor_level = AnalogIn(board.A1) # Pin to read the sensor
+
+pump = DigitalInOut(board.D2)  # Pin to control the pump
+pump.direction = Direction.OUTPUT
+pump.value = False # start with the pump turned off
+
+## reading interval
+INTERVAL = 30
+
+## liquid Level
+FULL = 25000
+
+def pump_on():
+    print("pump on, ", end = '')
+    pump.value = True
+
+def pump_off():
+    print("pump off")
+    pump.value = False
+
+def sensor_on():
+    print("sensor on, ", end = '')
+    sensor_enable.value = True
+
+def sensor_off():
+    print("sensor off")
+    sensor_enable.value = False
+
+def get_level():
+    sensor_on()
+    time.sleep(0.25) # Warm up time
+    #print("reading = ", end = '')
+    level = sensor_level.value
+    print(level, ", ", end = '')
+    sensor_off()
+    return level
+
+def fill_tank():
+    print("    ==Filling Tank==")
+    wlsv = get_level() # Water Level Sensor Value
+    while wlsv < FULL:
+        pump_on()
+        time.sleep(01.00) # Pump for a second before checking the level
+        wlsv = get_level()
+
+    print("    ==Tank is Full==")
+    pump_off()
+    sensor_off()
+
+##
+## START HERE
+##
+print("\nTank Monitor\n")
+print("Sensor will take a reading every", INTERVAL, "seconds and will\nturn on the pump when the level dips below", FULL,"\n")
+
+while True:
+    water_level = get_level()
+    if water_level < FULL:
+        fill_tank()
+    print("    ==sleep== ", INTERVAL)
+    time.sleep(INTERVAL)
+
